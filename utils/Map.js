e2a2e95b08029265cee9113c4ad6f9567a1427c2
Renato Lopes de Luna Freire <cad_rfreire@uolinc.com>
2014-05-23 15:13:59 -0300

    function that checks if an map is valid

--- utils/Map.js
+++ utils/Map.js
@@ -5,6 +5,19 @@
         var $protected = this;
         var $public = $protected.constructor.prototype;
 
+        $public.isValid = function (map) {
+            if (!map) {
+                return false;
+            }
+            if (map instanceof Array === false) {
+               return false;
+            }
+            if (!map[0] instanceof Object) {
+                return false;
+            }
+            return true;
+        };
+
         return $protected;
     }
 
